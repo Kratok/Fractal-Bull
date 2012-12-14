0e7882e748ca91b1bab165c35180c3dcc074819d
Kratok <greenj@unm.edu>
2012-12-14 14:05:08 -0800

    Create Processor.java
    When called this 'folds' the current fractal array, and records the direction of each turn in order from the beginning of the piece of paper.

---
+++ Processor.java
@@ -0,0 +1,53 @@
+
+public class Processor
+{
+  private static boolean run = true;
+  public int folds = 1; //now counts EACH crease. should make it as the power of creases made for(folds) {objects=*objects}
+  public byte[] lineObject = new byte[1000000]; {lineObject[0] = 2;}
+  byte[] buffer = new byte[1000000];
+  public int height; // count how many lines go up or down
+  public int width; // count how many lines go horizontally
+  void foldLine()
+  {
+    for (int i = 0; i < folds; i++) buffer[i] = lineObject[i];
+    for (int i = 0; i < folds; i++)
+    { 
+      //if(folds > lineObject.length)break;
+      if (buffer[i] == 3) buffer[i] = 0;
+      else buffer[i]++;
+    }
+    for (int i = 0; i < folds; i++)
+    {
+      lineObject[folds+i] = buffer[folds-1-i];
+    }
+    folds = folds * 2;
+  }
+  void cycleLine()
+  {
+    height = 0;
+    width = 0;
+    for (int i = 0; i < folds; i++)
+    {
+      switch (lineObject[i])
+      {
+        case 0: height--;
+        break;
+        case 1: width++;
+        break;
+        case 2: height++;
+        break;
+        case 3: width--;
+        break;
+      }
+    }
+    if(height == 0) height = 1;
+    System.out.println(height);
+    height = Math.abs(height);
+    width = Math.abs(width);
+  }
+}
+//Take the whole object by its beginning, rotate it 90 to the right, and add it to the object
+//for every fold create a byte that is 0 1 2 3 to lead the cardinal directions for a fold
+// Starting from the bottom (red) 
+// 2, 2 3, 2 3 0 3, "" 0 1 0 3, "" 0 1 2 1 0 1 0 3,
+// Start with last line object, flip all values, increment, insert
